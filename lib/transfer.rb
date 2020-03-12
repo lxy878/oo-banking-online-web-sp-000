3db31f2172bfdaccd9f3f26b5821d4a83e7a0e5f
Xiaoyi Liu <lxy878@gmail.com>
2020-03-12 11:09:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,8 +27,8 @@ class Transfer
 
   def reverse_transfer
     if @status == "complete"
-      @receiver -= @amount
-      @sender += @amount
+      @receiver.balance -= @amount
+      @sender.balance += @amount
       @status = "reversed"
     end
   end
